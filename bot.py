8fbb901b1ebaaab48c60c07c4abc7d8c212d7bae
Nicholas Latinette <nalrnw@umsystem.edu>
2022-04-10 06:49:38 -0500

    added slight configuablity

--- bot.py
+++ bot.py
@@ -14,31 +14,46 @@ client = discord.Client()
 
 task = discord.ext.tasks
 
-time_chosen = 0
 ready = True
 
-def choose_time():
-    time_chosen = 5.0
-    #time = random.randrange(2400, 12000, 200)
-    return time_chosen
-    ready = False
+lower_bound = 5
+upper_bound = 20
 
 
-@task.loop(seconds=10)
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == '!time one':
+        lower_bound = 5
+        upper_bound = 20
+    elif message.content == '!time two':
+        lower_bound = 100
+        upper_bound = 2000
+    elif message.content == '!time three':
+        lower_bound = 400
+        upper_bound = 12000
+    elif message.content == '!help':
+        channel = client.get_channel(962670927970201663)
+        await channel.send('!time one: 5 seconds to 20 seconds\n!time two: 100 secs to 2000 secs\n!time three: 400 secs to 12000 secs')
+    
+
+@task.loop(seconds=random.randrange(lower_bound,upper_bound,2))
 async def time():
-    channel = client.get_channel(962122924893040714)
-    await channel.send('@Pcheckers Posture check!')
+    channel = client.get_channel(962670927970201663)
+    await channel.send('@everyone Posture check!')
 
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
     if ready == True:
-        time_chosen = choose_time()
         await time.start()
     print("not test")
 
 
-    
+
 client.run(TOKEN)
 
 
