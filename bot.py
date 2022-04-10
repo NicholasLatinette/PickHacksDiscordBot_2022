4349b01c2db674627a9c13ac91ef0321f11b5e9e
Nicholas Latinette <nalrnw@umsystem.edu>
2022-04-10 07:20:40 -0500

    done!

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
+from email import message
 import os
 import discord
 from dotenv import load_dotenv
-from discord.ext import tasks
+from discord.ext import tasks, commands
 import random
 import asyncio
 
@@ -20,30 +21,41 @@ lower_bound = 5
 upper_bound = 20
 
 
+bot = commands.Bot(command_prefix='!')
 
 @client.event
-async def on_message(message):
+async def on_message(message, lb=0, ub=0):
+    channel = client.get_channel(962670927970201663)
+    global lower_bound
+    global upper_bound
     if message.author == client.user:
         return
-    
     if message.content == '!time one':
         lower_bound = 5
         upper_bound = 20
+        
+        await channel.send('Posture checks will now be between 5 and 20 seconds. *not reccomended*')
     elif message.content == '!time two':
         lower_bound = 100
         upper_bound = 2000
+        
+        await channel.send('Posture checks will now be between 100 and 2000 seconds.')
     elif message.content == '!time three':
-        lower_bound = 400
+        lower_bound = 300
         upper_bound = 12000
+        
+        await channel.send('Posture checks will now be between 400 and 12000 seconds.')
+   
     elif message.content == '!help':
-        channel = client.get_channel(962670927970201663)
-        await channel.send('!time one: 5 seconds to 20 seconds\n!time two: 100 secs to 2000 secs\n!time three: 400 secs to 12000 secs')
+        await channel.send('Configure when you get pinged by Posture Bot.\n!time one: 5 seconds to 20 seconds\n!time two: 100 secs to 2000 secs\n!time three: 400 secs to 12000 secs')    
+
     
 
-@task.loop(seconds=random.randrange(lower_bound,upper_bound,2))
+@task.loop(seconds=random.randrange(lower_bound,upper_bound))
 async def time():
     channel = client.get_channel(962670927970201663)
     await channel.send('@everyone Posture check!')
+
 
 @client.event
 async def on_ready():
